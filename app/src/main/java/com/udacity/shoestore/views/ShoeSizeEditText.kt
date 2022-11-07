b609d46119b7ef1fe717fdbb11b9d32a95c00de4
empty <empty87@yandex.ru>
2022-11-07 12:57:51 +0300

    removed param "size" in all project

--- app/src/main/java/com/udacity/shoestore/views/ShoeSizeEditText.kt
+++ app/src/main/java/com/udacity/shoestore/views/ShoeSizeEditText.kt
@@ -8,44 +8,30 @@ import androidx.databinding.InverseBindingMethod
 import androidx.databinding.InverseBindingMethods
 import com.google.android.material.textfield.TextInputEditText
 
-@BindingMethods(
-    BindingMethod(
-        type = ShoeSizeEditText::class,
-        attribute = "app:shoeSize",
-        method = "setShoeSize"
-    )
-)
+//@BindingMethods(
+//    BindingMethod(
+//        type = ShoeSizeEditText::class,
+//        attribute = "app:shoeSize",
+//        method = "setShoeSize"
+//    )
+//)
 
-@InverseBindingMethods(
-    InverseBindingMethod(
-        type = ShoeSizeEditText::class,
-        attribute = "app:shoeSize",
-        method = "getShoeSize",
-        event = "android:textAttrChanged"
-    )
-)
-class ShoeSizeEditText : TextInputEditText {
-    constructor(
-        context: Context
-    ) : super(context)
 
-    constructor(
-        context: Context,
-        attributeSet: AttributeSet?
-    ) : super(context, attributeSet)
+//class ShoeSizeEditText : TextInputEditText {
+//    constructor(
+//        context: Context
+//    ) : super(context)
+//
+//    constructor(
+//        context: Context,
+//        attributeSet: AttributeSet?
+//    ) : super(context, attributeSet)
+//
+//    constructor(
+//        context: Context,
+//        attributeSet: AttributeSet?,
+//        defStyleAttr: Int
+//    ) : super(context, attributeSet, defStyleAttr)
+//
 
-    constructor(
-        context: Context,
-        attributeSet: AttributeSet?,
-        defStyleAttr: Int
-    ) : super(context, attributeSet, defStyleAttr)
-
-    fun setShoeSize(size: Double) {
-        this.setText(size.toString())
-    }
-
-    fun getShoeSize(): Double {
-        val sizeAsString = this.text?.toString() ?: "0.0"
-        return sizeAsString.toDouble()
-    }
-}
+//}
